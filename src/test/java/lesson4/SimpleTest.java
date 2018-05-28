3f3e243d94822c127e295d94aa7ea3a0c0dc0157
Feofelaktiya <kxc$#2FV>
2018-05-29 01:58:00 +0300

    All project including:
    1) First homework in hw1 package
    2) Exercises from lessons
    3) gitignore file
    4) pom file
    5) chromedriver.exe file

---
+++ src/test/java/lesson4/SimpleTest.java
@@ -0,0 +1,43 @@
+package lesson4;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+
+public class SimpleTest {
+
+    @Test
+    public void simpleSeleniumTest(){
+       // System.setProperty("webdriver.chrome.driver");
+        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+        chromePrefs.put("download_default_directory", "target");
+
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("pref", chromePrefs);
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to("https://jdi-framework.github.io/tests/index.htm");
+
+        Assert.assertEquals(driver.getTitle(),"Index Page"); //класс проверок TestNG
+
+        WebElement userIcon = driver.findElement(By.cssSelector(".fa-user"));
+        userIcon.click();
+        driver.findElement(By.cssSelector("#Login")).sendKeys("epam");
+        driver.findElement(By.cssSelector("#Password")).sendKeys("1234");
+        driver.findElement(By.cssSelector(".form-horizontal button[type = 'submit']")).click();
+        WebElement userName = driver.findElement(By.cssSelector(".profile-photo span"));
+        Assert.assertEquals(userName.getText(), "PITER CHAILOVSKII");
+
+        driver.close();
+
+    }
+
+
+}
